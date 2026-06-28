d4f5b2941aaaf315fe9007ffc1083c49086258e4
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-06-28 22:11:37 +0530

    Navbar + Homepage UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { Switch } from '#/components/ui/switch'
 import { authClient } from '#/lib/auth-client'
+import ThemeToggle from '#/components/ThemeToggle'
+import { getSession } from '#/lib/auth.function'
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({ 
+  beforeLoad:async({location}) => {
+    const session = await getSession();
+
+    if(!session){
+      throw redirect({
+        to:"/login",
+        search:{redirect:location.href}
+      })
+    }
+
+    return {user:session.user}
+  },
+  component: App
+})
 
 function App() {
-  const {data} = authClient.useSession()
-  console.log(data)
+
   return (
-    <div>
-      <Switch/>
-    </div>
+    <main className='min-h-screen pt-24 pb-12 px-4'>
+      <div className="max-w-4xl mx-auto">
+        <h1>Hello World!</h1>  
+      </div> 
+    </main>
   )
 }
